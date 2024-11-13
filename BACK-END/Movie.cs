a6c43dc70b9c6e0cce2d968ea9d79ca27765bb2a
jaimeescartinsv <a27421@svalero.com>
2024-11-13 18:47:51 +0100

    añadido duración a las películas

--- BACK-END/Movie.cs
+++ BACK-END/Movie.cs
@@ -3,5 +3,6 @@ public class Movie
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public int Duration { get; set; }
     public string ImageUrl { get; set; }
 }
