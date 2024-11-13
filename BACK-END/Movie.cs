ac46026367352c38ffd963e9c19b137bd7090391
jaimeescartinsv <a27421@svalero.com>
2024-11-13 20:00:42 +0100

    eliminado back

--- BACK-END/Movie.cs
+++
@@ -1,8 +0,0 @@
-public class Movie
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public int Duration { get; set; }
-    public string ImageUrl { get; set; }
-}
