69881b1a24bb585739ad4dace45323116fbebeac
gaabsito <157628366+gaabsito@users.noreply.github.com>
2024-11-14 19:57:56 +0100

    act footer-options

--- FRONT-END/source/js/header-y-footer.js
+++ FRONT-END/source/js/header-y-footer.js
@@ -2,3 +2,21 @@
 document.querySelector('.header__menu-toggle').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('nav--menu-active');
 });
+
+
+window.addEventListener('scroll', function() {
+    const footerTop = document.querySelector('.footer__top');
+    const footerBottom = document.querySelector('.footer__bottom');
+    const footerTopHeight = footerTop.offsetHeight;
+    const footerBottomTop = footerBottom.offsetTop;
+    const scrollPosition = window.scrollY + window.innerHeight;
+  
+    // Cuando el usuario hace scroll hacia abajo, mantener el footer-top centrado
+    if (scrollPosition < footerBottomTop) {
+      footerTop.classList.remove('footer__top-sticky');
+    } else {
+      // Al llegar al final del contenido, hacer que el footer-top se quede encima del footer__bottom
+      footerTop.classList.add('footer__top-sticky');
+    }
+  });
+  
