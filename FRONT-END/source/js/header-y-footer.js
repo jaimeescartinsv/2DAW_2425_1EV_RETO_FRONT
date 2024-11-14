9f26db12956971e87fc82a69d2b74a0402881a6a
gaabsito <157628366+gaabsito@users.noreply.github.com>
2024-11-14 19:40:15 +0100

    modificado mobile first

---
+++ FRONT-END/source/js/header-y-footer.js
@@ -0,0 +1,4 @@
+// Función para activar/desactivar el menú hamburguesa
+document.querySelector('.header__menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav').classList.toggle('nav--menu-active');
+});
