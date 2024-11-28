36b6794bc9c42b0341c4f70e7de5c5b9de51a8ae
jaimeescartinsv <157627939+jaimeescartinsv@users.noreply.github.com>
2024-11-28 19:39:14 +0100

    comit general linkeado y htmls

---
+++ FRONT-END/source/js/sobre-nosotros.js
@@ -0,0 +1,50 @@
+// Función para activar/desactivar el menú hamburguesa
+document.querySelector('.header__menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav').classList.toggle('nav--menu-active');
+});
+
+
+
+
+window.addEventListener('scroll', function () {
+  const footerTop = document.querySelector('.footer__top');
+  const footer = document.querySelector('.footer');
+  const footerTopHeight = footerTop.offsetHeight;
+  const footerOffsetTop = footer.offsetTop; // Posición superior del footer
+  const viewportHeight = window.innerHeight;
+  const scrollPosition = window.scrollY;
+
+  // Si el scroll aún no llega al footer
+  if (scrollPosition + viewportHeight < footerOffsetTop) {
+      footerTop.style.position = 'fixed';
+      footerTop.style.bottom = '0';
+      footerTop.style.top = 'auto';
+  } 
+  // Si el scroll llega al footer
+  else {
+      footerTop.style.position = 'absolute';
+      footerTop.style.bottom = 'auto';
+      footerTop.style.top = `${footerOffsetTop - footerTopHeight}px`; // Ajusta justo encima del footer
+  }
+});
+
+
+
+
+
+
+
+
+
+
+  
+  document.querySelector('.header__menu-toggle').addEventListener('click', function () {
+    document.querySelector('.nav').classList.add('nav--menu-active');
+});
+
+document.querySelector('.nav__close-button').addEventListener('click', function () {
+    document.querySelector('.nav').classList.remove('nav--menu-active');
+});
+
+
+  
