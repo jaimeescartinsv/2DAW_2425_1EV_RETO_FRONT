6e7ae3042c376d96650577bf56b4ad24afda1ea8
jaimeescartinsv <a27421@svalero.com>
2024-11-30 13:36:12 +0100

    Update gulpfile.js

--- FRONT-END/gulpfile.js
+++ FRONT-END/gulpfile.js
@@ -8,16 +8,10 @@ gulp.task('compilar-sass', function() {
         .pipe(gulp.dest('dist'));  
 });
 
-// Tarea para copiar HTML a carpeta dist
-gulp.task('copiar-html', function() {
-    return gulp.src('source/html/*.html') // Ruta de los HTML
-        .pipe(gulp.dest('dist')); // Ruta de salida
-});
-
 // Tarea de vigilancia (watch)
 gulp.task('watch', function() {
     gulp.watch('source/styles/**/*.scss', gulp.series('compilar-sass'));
 });
 
 // Tarea por defecto que ejecuta ambas tareas
-gulp.task('default', gulp.series('compilar-sass', 'copiar-html'));
+gulp.task('default', gulp.series('compilar-sass', 'watch'));
